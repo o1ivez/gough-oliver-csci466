784cdb67fcf53e0d497914b888a22ab9c115e465
Oliver Gough <123779859+o1ivez@users.noreply.github.com>
2024-11-22 21:22:30 -0700

    Update server.py

--- PA4/server.py
+++ PA4/server.py
@@ -1,16 +1,41 @@
-import random
 import hashlib
 import socket
 
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
+#encryption set up
 key = "0123456789ABCDEF"
 sharedSecret = "csci466"
+block_cipher = AES.new(key.encode(), AES.MODE_ECB) #create block cypher
+
 
 #set up socket
 port = 9000
 host = socket.gethostname()
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind((host, port))
+serverSocket.listen(1)
+connection, addr = serverSocket.accept()
 
+while(True):
+    #get message
+    encodedMessage = connection.recv(1024)
+    print("Encrypted Message: " + str(encodedMessage) + "\n")
+    decodedMessage = (unpad(block_cipher.decrypt(encodedMessage),32)).decode()
+    print("Decrypted Message: " + decodedMessage + "\n")
+    
+    #hash code
+    MACRecv = connection.recv(1024).decode()
+    print("MAC recieved: " + MACRecv)
+    mPlusS = decodedMessage + sharedSecret
+    mPlusSBytes = mPlusS.encode()
+    digest = hashlib.sha256(mPlusSBytes)
+    calcHash = str(digest.hexdigest())
+    print("\nComputing H(m+s) on our end.... \n")
+    print("Value of (m+s): " + calcHash + "\n")
+    if(calcHash == MACRecv):
+        print("Hashes match!! Accept")
+    else:
+        print("Hashes do not match!! Reject")
+    print("--------------------------------------------")
