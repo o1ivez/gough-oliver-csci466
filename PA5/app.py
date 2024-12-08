c1875dacdcddf5f879edb63c4b4440063acb192b
Oliver Gough <123779859+o1ivez@users.noreply.github.com>
2024-12-08 11:28:06 -0700

    Create app.py

---
+++ PA5/app.py
@@ -0,0 +1,39 @@
+from flask import Flask,request
+import random
+import datetime
+
+#read in funfacts csv
+facts = []
+f = open('facts.csv','r', encoding='utf-8')
+for i in f:
+    i = i.replace("\n", "")
+    i = i.replace('"', "")
+    facts.append(i)
+
+#flask
+app = Flask(__name__)
+
+@app.route('/fact')
+def fact():
+    j= 0 
+    printStr = ""
+    tempFact = facts
+    i = int(request.headers['Amount'])
+    while (j < i):
+        randChoice = random.choice(tempFact)
+        printStr = str(randChoice) + "\n\n" + printStr
+        tempFact.remove(str(randChoice))
+        j = j + 1
+        print("hI")
+    return "\n" + printStr
+
+@app.route('/info')
+def info():
+    current_time = str(datetime.datetime.now())
+    userAgent = str(request.user_agent)
+    method = str(request.method)
+    printStr = "\n" + "Time: " + current_time + "\n" + "User Agent: " + userAgent + "\n" + "Tranmission type: " + method + "\n"
+    return printStr
+
+if __name__ == '__main__':
+    app.run()
